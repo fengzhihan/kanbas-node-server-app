6102a1db78f66d1676f69a1cd04b953967bc4184
fengzhihan <fengzhihan98@gmail.com>
2023-11-20 13:20:52 +0800

    node app a5 new

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from './modules/routes.js';
+import AssignmentRoutes from "./assignments/routes.js";
 
 
 const app = express();
@@ -12,7 +13,9 @@ app.use(cors());
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
+AssignmentRoutes(app);
 Hello(app)
 Lab5(app);
+
 
 app.listen(process.env.PORT || 4000);
